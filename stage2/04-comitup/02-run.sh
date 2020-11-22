352e3cca2efc05514fec4e2e8dbd3cda82343f77
Michael Houghton <mike@depthoffocus.co.uk>
2020-11-22 06:18:17 +0000

    Override comitup.conf with our copy

--- stage2/04-comitup/02-run.sh
+++ stage2/04-comitup/02-run.sh
@@ -3,3 +3,5 @@
 for conn in dhcp static; do
     install -m 600 files/${conn}.nmconnection ${ROOTFS_DIR}/etc/NetworkManager/system-connections/
 done
+
+install -m 600 files/comitup.conf ${ROOTFS_DIR}/etc/
