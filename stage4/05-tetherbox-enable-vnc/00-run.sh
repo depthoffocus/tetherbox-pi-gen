f7697b6297a6487c191f87d00f9aa738712bfaa1
Michael Houghton <mike@depthoffocus.co.uk>
2020-11-23 03:11:35 +0000

    VNC HDMI. Plain VNC authentication. New user password. Comitup AP password (not currently working)

--- stage4/05-tetherbox-enable-vnc/00-run.sh
+++ stage4/05-tetherbox-enable-vnc/00-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
+VNC_PASSWD="ch33se!"
+
 on_chroot << EOF
 	raspi-config nonint do_vnc 0
-	raspi-config nonint do_resolution 2 82
+	echo "$VNC_PASSWD" | vncpasswd -service
+	echo "Authentication=VncAuth" > /etc/vnc/config.d/common.custom
 EOF
+
+install -m 600 files/config.txt "${ROOTFS_DIR}/boot/"
+
